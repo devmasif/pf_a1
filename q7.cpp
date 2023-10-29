51b39a98998350dcd3025c9e7c33ad90a11c770c
devmasif <143322749+devmasif@users.noreply.github.com>
2023-10-29 17:58:13 +0500

    q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,24 +1,27 @@
+//Muhammad Asif
+//23I-0648
+//CS-G
+//Assignment # 1
+//Question # 7
+
 #include<iostream>
 #include<bitset>
-#include<iomanip>
 using namespace std;
 int main(){
-int n;
-cin>>n;
-cout<<bitset<16>(n)<<endl;
-cout<<bitset<16>(n^(n/2))<<endl;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //Declare variable
+    unsigned int n;
+    
+    //Ask the user to enter a 16-bit integer
+    cout<<"Enter any non-zero (16-bit) integer: "<<endl; 
+    cin>>n;
+    cout<<"\n";
+    
+    //Printing 16-bit binary number
+    cout<<"Binary code of "<<n<<" is : "<<bitset<16>(n)<<"\n\n";
+    
+    //Printing gray code of entered binary number (by taking XOR between Entered number and right shift by 1 of the same number)
+    cout<<"Gray code of "<<n<<" is : "<<bitset<16>(n^(n>>1))<<"\n\n"; //
+	
+    return 0;      
 }

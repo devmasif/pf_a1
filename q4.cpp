5f3d5ee761f535f218bec2c4cc77d5cdc32ceff4
devmasif <143322749+devmasif@users.noreply.github.com>
2023-10-29 17:48:53 +0500

    q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,47 +1,39 @@
+//Muhammad Asif
+//23I-0648
+//CS-G
+//Assignment # 1
+//Question # 4
+
 #include<iostream>
 #include<iomanip>
-#include<cmath>
+
 using namespace std;
 int main(){
+    //Variable Declaration
     int N1,N2,N3,N4,N5;
-    cout<<"N1: ";
+    
+    //Ask the user to enter values 
+    cout<<"\n Enter the numbers  \n";
+    cout<<" N1: ";
     cin>>N1;
-    cout<<"N2: ";
+    cout<<" N2: ";
     cin>>N2;
-    cout<<"N3: ";
+    cout<<" N3: ";
     cin>>N3;
-    cout<<"N4: ";
+    cout<<" N4: ";
     cin>>N4;
-    cout<<"N5: ";
+    cout<<" N5: ";
     cin>>N5;
-    cout<<"N1: "<<setw(N1)<<setfill('*')<<""<<"\n";
-    cout<<"N2: "<<setw(N2)<<setfill('*')<<""<<"\n";
-    cout<<"N3: "<<setw(N3)<<setfill('*')<<""<<"\n";
-    cout<<"N4: "<<setw(N4)<<setfill('*')<<""<<"\n";
-    cout<<"N5: "<<setw(N5)<<setfill('*')<<""<<"\n";
+    cout<<"\n";
+    
+    //code
+    cout<<" N1: "<<setw(N1)<<setfill('*')<<""<<"\n";
+    cout<<" N2: "<<setw(N2)<<setfill('*')<<""<<"\n";
+    cout<<" N3: "<<setw(N3)<<setfill('*')<<""<<"\n";
+    cout<<" N4: "<<setw(N4)<<setfill('*')<<""<<"\n";
+    cout<<" N5: "<<setw(N5)<<setfill('*')<<""<<"\n";
 
-    cout<<"\n\n";
+    cout<<"\n";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
+    return 0;
 }

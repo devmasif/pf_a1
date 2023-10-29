218955c97d92ba77004b6afe5b2da0dd23caa4ed
devmasif <143322749+devmasif@users.noreply.github.com>
2023-10-29 17:40:57 +0500

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,26 +1,42 @@
+//Muhammad Asif
+//23I-0648
+//CS-G
+//Assignment # 1
+//Question # 2
+
 #include<iostream>
 #include<cmath>
-#include<iomanip>
 
 using namespace std;
 int main(){
-unsigned int a1,N1,N2,N3,D1,D2,D3,True,False;
-cout<<"Enter First Number\n";
+//Declaring variables 
+ int N1,N2,N3,D1,D2,D3,result1,result2,invalid;
+
+//Ask the user to enter a number 
+cout <<  "\n Enter only Positive numbers \n";                              
+cout<<"\n Enter First Number : ";
 cin>>N1;
-cout<<"Enter Second Number\n";
+cout<<"\n Enter Second Number : ";
 cin>>N2;
-cout<<"Enter Third Number\n";
+cout<<"\n Enter Third Number : ";
 cin>>N3;
+
+//Using %(mod) to get right most digit 
 D1=N1%10;
 D2=N2%10;
 D3=N3%10;
-cout<<D1<<endl;
-cout<<D2<<endl;
-cout<<D3<<endl;
-bool result;
-result=(D1==D2)||(D2==D3)||(D1==D3);
-cout<<boolalpha<<result;
-cout<<"\n";
+
+
+//When numbers entered are >0 and 2 of the rightmost digits are equal then output is true  
+result1=((D1==D2)||(D2==D3)||(D1==D3))&&((N1>=0)&&(N2>=0)&&(N3>=0)) && cout<<"\n The result is : true" ;
+
+//when numbers entered are >0 and all the right most digits are not equal then output is false 
+result2=(D1!=D2)&&(D1!=D3)&&(D2!=D3)&&((N1>=0)&&(N2>=0)&&(N3>=0)) && cout<<"\n The result is : false ";
+
+//when number entered are <0 then output is invalid entry (even if 2 of them are equal) 
+invalid= ((N1<0)||(N2<0)||(N3<0))  && cout<<"\n The numbers entered are invalid ";
+
+cout<<"\n\n";
 
 return 0;
 }

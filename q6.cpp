ac6f8ef68d452b0edb9ce77ae7e5eba42670a748
devmasif <143322749+devmasif@users.noreply.github.com>
2023-10-29 17:56:41 +0500

    q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -8,25 +8,25 @@
 #include<bitset>
 using namespace std;
 int main(){
-//Variable declaration (Unsigned use for 32 bit input)
+//Variable declaration 
 unsigned int n,n1,n2,n3;
 
-//Ask the user to enter value 
-cout <<"\n Enter a 32 bit positive number : ";
+//Ask the user to enter the value 
+cout <<"\n Enter a 32-bit positive number: ";
 cin>>n;
 
-//bitset to 8 will print gate 1 ( 8 right most digits)
+//bitset to 8 will print gate 1 ( 8 right-most digits)
 cout<<"\n Gate 1 = "<<bitset<8>(n)<<"\n\n";
 
-//right shift n by 8 and set bitset to 8 to get gate 2
+//right shift n by 8 and set bit set to 8 to get gate 2
 n1=(n>>8);
 cout<<" Gate 2 = "<<bitset<8>(n1)<<"\n\n";
 
-//right shift n by 16 and set bitset to 8 to get gate 3
+//right shift n by 16 and set bit set to 8 to get gate 3
 n2=(n>>16);
 cout<<" Gate 3 = "<<bitset<8>(n2)<<"\n\n";
 
-//right shift n by 24 and set bitset to 8 to get public constant
+//right shift n by 24 and set bit set to 8 to get public constant
 n3=(n>>24);
 cout<<" Public  Constant = "<<bitset<8>(n3)<<"\n\n";
 

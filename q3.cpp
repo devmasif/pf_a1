3a7c1f87cb9a1f18f9b683019e8c2b23ae7ad0c7
devmasif <143322749+devmasif@users.noreply.github.com>
2023-10-29 17:47:47 +0500

    q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -11,8 +11,9 @@ using namespace std;
 int main()
 {
 //Variable declaration
-int c=30,b,p;
-float N,i;
+const int c =30;
+int b,p;
+float N, i;
 //Ask the user to Enter Values
 cout<<"Enter the balance(b):  "<<endl;
 cin>>b;
@@ -20,12 +21,11 @@ cout<<"Enter Monthly Payment(p):  "<<endl;
 cin>>p;
 cout<<"Enter Yearly Interest Rate(i):  "<<endl;
 cin>>i;
-cout<<"Enter value of c (30) :"<<endl; 
-cin>>c;
+
 
 //N=-1/c*((ln(1+b/p*(1-(1+i)^c)))/ln(1+i)) ----------------->  eq(1)
 
-//Declare different parts of equation (1) equal to  different variables(v1,v2,etc...) and then declaring them as numerator,denominator.)
+//Declare different parts of equation (1) equal to  different variables(v1,v2, etc...) and then declare them as numerator, denominator.)
 float v1,v2,v3,v4,v5,Numerator,Denominator;
 i=i/365;
 v1 = (-1.0);

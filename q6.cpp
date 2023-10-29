48cfa7ce063ee5af5e2177b123fe32496f809b9b
devmasif <143322749+devmasif@users.noreply.github.com>
2023-10-29 17:55:45 +0500

    q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -1,16 +1,39 @@
+//Muhammad Asif
+//23I-0648
+//CS-G
+//Assignment # 1
+//Question # 6
+
 #include<iostream>
 #include<bitset>
-#include<iomanip>
-#include<string>
 using namespace std;
 int main(){
-int n;
+//Variable declaration (Unsigned use for 32 bit input)
+unsigned int n,n1,n2,n3;
+
+//Ask the user to enter value 
+cout <<"\n Enter a 32 bit positive number : ";
 cin>>n;
 
-cout<<bitset<4>(n)<<endl;
-n= bitset<1>(n);
+//bitset to 8 will print gate 1 ( 8 right most digits)
+cout<<"\n Gate 1 = "<<bitset<8>(n)<<"\n\n";
 
-//remaining implementatiaon working on
+//right shift n by 8 and set bitset to 8 to get gate 2
+n1=(n>>8);
+cout<<" Gate 2 = "<<bitset<8>(n1)<<"\n\n";
+
+//right shift n by 16 and set bitset to 8 to get gate 3
+n2=(n>>16);
+cout<<" Gate 3 = "<<bitset<8>(n2)<<"\n\n";
+
+//right shift n by 24 and set bitset to 8 to get public constant
+n3=(n>>24);
+cout<<" Public  Constant = "<<bitset<8>(n3)<<"\n\n";
+
+//Key is equal to XOR of gate1,gate2,gate3 and public constant
+cout<<" Key = "<<bitset<8>(n^n1^n2^n3)<<"\n\n";
 
 
+
+return 0;
 }
